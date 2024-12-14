e20759abcef569ae5a0bc8f2f144e5b780294ec5
juan pablo lopez <jua.lopezp@duocuc.cl>
2024-12-13 23:13:12 -0300

    sin pruebas

--- src/app/page/agregar-vehiculo/agregar-vehiculo.page.spec.ts
+++ src/app/page/agregar-vehiculo/agregar-vehiculo.page.spec.ts
@@ -1,11 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AgregarVehiculoPage } from './agregar-vehiculo.page';
+import { HttpClientModule } from '@angular/common/http';  // Importa HttpClientModule
+import { ApiService } from 'src/app/service/api.service'; // Asegúrate de importar tu ApiService
+import { RouterTestingModule } from '@angular/router/testing';  // Si usas routing en el componente
 
 describe('AgregarVehiculoPage', () => {
   let component: AgregarVehiculoPage;
   let fixture: ComponentFixture<AgregarVehiculoPage>;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule,  // Importa el módulo HttpClientModule
+        RouterTestingModule  // Importa si estás usando enrutamiento en el componente
+      ],
+      declarations: [ AgregarVehiculoPage ],
+      providers: [ ApiService ]  // Proveedor del ApiService si es necesario
+    })
+    .compileComponents();
+
     fixture = TestBed.createComponent(AgregarVehiculoPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/page/crearuser/crearuser.page.spec.ts
+++ src/app/page/crearuser/crearuser.page.spec.ts
@@ -1,11 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { crearuserpage } from './crearuser.page';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { environment } from 'src/environments/environment'; // Asegúrate de tener esta importación para usar la configuración de Firebase
+import { AngularFireModule } from '@angular/fire/compat';
 
 describe('CrearuserPage', () => {
   let component: crearuserpage;
   let fixture: ComponentFixture<crearuserpage>;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        AngularFireModule.initializeApp(environment.firebaseConfig), // Inicializa Firebase con la configuración
+        AngularFireAuthModule  // Importa AngularFireAuthModule para la autenticación
+      ],
+      declarations: [crearuserpage] // Declara el componente que estás probando
+    }).compileComponents();
+
     fixture = TestBed.createComponent(crearuserpage);
     component = fixture.componentInstance;
     fixture.detectChanges();
